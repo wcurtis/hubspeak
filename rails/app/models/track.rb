95ec31f34baeb10c058bb76bab44a9661088174a
Bill Curtis <billcurt@gmail.com>
2014-06-08 11:34:10 -0400

    Added validator to track

--- rails/app/models/track.rb
+++ rails/app/models/track.rb
@@ -1,3 +1,5 @@
 class Track < ActiveRecord::Base
   belongs_to :soundboard
+
+  validates :soundboard, presence: true
 end
--- rails/app/serializers/soundboard_serializer.rb
+++ rails/app/serializers/soundboard_serializer.rb
@@ -1,3 +1,4 @@
 class SoundboardSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_many :track
 end
