24c44eb886da8d34e4515d60a57fce2eb6855bb4
Bill Curtis <billcurt@gmail.com>
2014-06-08 17:23:54 -0400

    Now hiding/showing upload button based on soundbite presence

--- ember/app/controllers/track.js
+++ ember/app/controllers/track.js
@@ -1,8 +1,15 @@
 var TrackController = Ember.ObjectController.extend({
 
+  hasSoundbite: function() {
+    return !Ember.isNone(this.get('soundbite'));
+  }.property('soundbite'),
+
   actions: {
     didUploadFile: function(data) {
-      debugger;
+      var soundbite = this.store.createRecord('soundbite', data.soundbite);
+      var track = this.get('model');
+
+      track.set('soundbite', soundbite);
     }
   }
 
