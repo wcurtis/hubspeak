5bedf6c37a9946bf119116939ba9369d73b6e6f9
Bill Curtis <wcurtis@sweettoothhq.com>
2014-06-07 23:17:10 -0400

    Configured broccoli to use compile bootstrap fonts into public assets

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,14 +2,14 @@ var IndexController = Ember.Controller.extend({
 
   actions: {
     createSoundboard: function() {
-      var newSoundboard = this.store.createRecord('soundboard', {
-        id: 2,
-        name: "New board"
-      });
+      // var newSoundboard = this.store.createRecord('soundboard', {
+      //   id: 2,
+      //   name: "New board"
+      // });
 
       // TODO: Save board
 
-      this.transitionToRoute('soundboard', newSoundboard);
+      this.transitionToRoute('soundboard', 1);
     }
   }
 
