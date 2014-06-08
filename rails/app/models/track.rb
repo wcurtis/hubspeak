133f4aaa13febd54b5632ce87f1aa431965dbde3
Bill Curtis <billcurt@gmail.com>
2014-06-08 19:49:48 -0400

    Added home button

--- rails/app/models/track.rb
+++ rails/app/models/track.rb
@@ -3,4 +3,10 @@ class Track < ActiveRecord::Base
   has_one :soundbite
 
   validates :soundboard, presence: true
+
+  before_save :default_values
+
+  def default_values
+    self.play_count ||= 0
+  end
 end
