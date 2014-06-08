c9f6aaf56d14898cb611a0ba75445b867ba50ee7
Bill Curtis <billcurt@gmail.com>
2014-06-08 17:37:51 -0400

    Now returning audio url in api

--- rails/app/serializers/soundbite_serializer.rb
+++ rails/app/serializers/soundbite_serializer.rb
@@ -1,6 +1,16 @@
 class SoundbiteSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :url
   has_one :track
+
+  def url
+    result = object.audio.url(:original)
+
+    if Rails.env.development?
+      result = "http://local-api.hubspeak.com:7000#{result}"
+    end
+
+    result
+  end
 
   embed :ids, include: false
 end
